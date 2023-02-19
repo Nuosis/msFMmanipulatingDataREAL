fc92c5b092bf6828bf351412f757f7ec41907e96
Marcus Swift <marcus@claritybusinesssolutions.ca>
2023-02-18 16:15:37 -0800

    objectMethods

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 import { companiesData } from "./companies-data";
+import { extraRecord } from "./extra-record";
+console.log("Extra" , extraRecord);
 
 const before = document.getElementById("before");
 before.innerHTML = JSON.stringify(companiesData, null, 2);
 
-const manipulatedData = [];
-//TODO: add the manipulated data array to the 'after' div.
+//const manipulatedData = Object.keys(extraRecord.fieldData);
+extraRecord.fieldData.Country = "USA";
+const manipulatedData = extraRecord.fieldData;
+//TODO: add the manipulated data array to the 'after' div. (ms: DONE)
+const after = document.getElementById("after");
+after.innerHTML = JSON.stringify(manipulatedData, null, 2);
