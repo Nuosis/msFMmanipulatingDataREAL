4a75c1cd384abaac7f5d2f815a65924cfb1bc2b8
Marcus Swift <marcus@claritybusinesssolutions.ca>
2023-02-18 16:16:17 -0800

    objectmethod2

--- src/index.js
+++ src/index.js
@@ -5,9 +5,12 @@ console.log("Extra" , extraRecord);
 const before = document.getElementById("before");
 before.innerHTML = JSON.stringify(companiesData, null, 2);
 
+//OBJECT
 //const manipulatedData = Object.keys(extraRecord.fieldData);
-extraRecord.fieldData.Country = "USA";
-const manipulatedData = extraRecord.fieldData;
+//extraRecord.fieldData.Country = "USA";
+//extraRecord.fieldData.cityState = `${extraRecord.fieldData.City}, ${extraRecord.fieldData.State}`;
+//delete extraRecord.fieldData.Id;
+const manipulatedData = extraRecord;
 //TODO: add the manipulated data array to the 'after' div. (ms: DONE)
 const after = document.getElementById("after");
 after.innerHTML = JSON.stringify(manipulatedData, null, 2);
