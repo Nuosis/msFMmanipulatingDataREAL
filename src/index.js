942a60b8f8ed51f5eee71434cc318402c00a0fcd
Marcus Swift <marcus@claritybusinesssolutions.ca>
2023-02-18 16:19:51 -0800

    afterAdded

--- src/index.js
+++ src/index.js
@@ -4,4 +4,6 @@ const before = document.getElementById("before");
 before.innerHTML = JSON.stringify(companiesData, null, 2);
 
 const manipulatedData = [];
-//TODO: add the manipulated data array to the 'after' div.
+//TODO: add the manipulated data array to the 'after' div.(ms: DONE)
+const after = document.getElementById("after");
+after.innerHTML = JSON.stringify(manipulatedData, null, 2);
